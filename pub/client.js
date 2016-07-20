1e345a503b834fcc291d78251379c64f87995371
William <devwb@outlook.com>
2016-07-20 18:57:24 +0100

    Added basic multiplayer

--- pub/client.js
+++ pub/client.js
@@ -4,18 +4,10 @@
 
 var socket = io('http://localhost:80');
 
-var ctx = document.getElementById("ctx").getContext("2d");
+var canvas = document.getElementById("canvas");
+var ctx = document.getElementById("canvas").getContext("2d");
 ctx.font = '30px Arial';
 
-
-
-socket.emit('testy', "desu");
-
-socket.on("sugoi", function() {
-    console.log("hah gay");
-});
-
-/*
 document.onmousedown = function(event){socket.emit('keyPress',{inputId:'attack',state:true});
 };
 document.onmouseup = function(event){
@@ -26,26 +18,38 @@ document.onmousemove = function(event){
     var y = -250 + event.clientY - 8;
     var angle = Math.atan2(y,x) / Math.PI * 180;
     socket.emit('keyPress',{inputId:'mouseAngle',state:angle});
-}
- document.onkeydown = function(event){
- if(event.keyCode === 68)	//d
- socket.emit('keyPress',{inputId:'right',state:true});
- else if(event.keyCode === 83)	//s
- socket.emit('keyPress',{inputId:'down',state:true});
- else if(event.keyCode === 65) //a
- socket.emit('keyPress',{inputId:'left',state:true});
- else if(event.keyCode === 87) // w
- socket.emit('keyPress',{inputId:'up',state:true});
+};
+document.onkeydown = function(event){
+    if(event.keyCode === 68)	//d
+        socket.emit('keyPress',{inputId:'right',state:true});
+    else if(event.keyCode === 83)	//s
+        socket.emit('keyPress',{inputId:'down',state:true});
+    else if(event.keyCode === 65) //a
+        socket.emit('keyPress',{inputId:'left',state:true});
+    else if(event.keyCode === 87) // w
+        socket.emit('keyPress',{inputId:'up',state:true});
 
- };
- document.onkeyup = function(event){
- if(event.keyCode === 68)	//d
- socket.emit('keyPress',{inputId:'right',state:false});
- else if(event.keyCode === 83)	//s
- socket.emit('keyPress',{inputId:'down',state:false});
- else if(event.keyCode === 65) //a
- socket.emit('keyPress',{inputId:'left',state:false});
- else if(event.keyCode === 87) // w
- socket.emit('keyPress',{inputId:'up',state:false});
- };
-*/
+};
+document.onkeyup = function(event){
+    if(event.keyCode === 68)	//d
+        socket.emit('keyPress',{inputId:'right',state:false});
+    else if(event.keyCode === 83)	//s
+        socket.emit('keyPress',{inputId:'down',state:false});
+    else if(event.keyCode === 65) //a
+        socket.emit('keyPress',{inputId:'left',state:false});
+    else if(event.keyCode === 87) // w
+        socket.emit('keyPress',{inputId:'up',state:false});
+};
+
+
+socket.on('nP',function(data){
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+
+    for(var i = 0 ; i < data.length; i++) {
+        console.log('rgb = ' + data[i].r + data[i].g + data[i].b);
+        ctx.fillStyle = 'rgb(' + data[i].r + ',' + data[i].g + ',' + data[i].b + ')';
+        ctx.fillRect(data[i].x, data[i].y, 100, 60);
+    }
+});
--- zpk.js
+++ zpk.js
@@ -13,10 +13,46 @@ app.use('/pub',express.static(__dirname + '/pub'));
 serv.listen(process.env.PORT || 80);
 console.log("Server started.");
 
+var socketList = {};
+
 var io = require('socket.io')(serv,{});
+io.sockets.on('connection', function(socket){
+	socket.id = Math.random();
+	console.log("Client " + socket.id + " connected.");
+	socket.x = 0;
+	socket.y = 0;
+	socket.r = Math.floor(Math.random()*(255)+1);
+	socket.g = Math.floor(Math.random()*(255)+1);
+	socket.b = Math.floor(Math.random()*(255)+1);
 
-io.sockets.on('connection', function(socket) {
-	console.log("kawaii connection you got there sempai");
+	socketList[socket.id] = socket;
 
-	socket.emit('sugoi');
+	socket.on('disconnect',function(){
+		delete socketList[socket.id];
+	});
+
 });
+
+setInterval(function(){
+	var pack = [];
+	for(var s in socketList){
+		var socket = socketList[s];
+		socket.x++;
+		socket.y++;
+		pack.push({
+			x:socket.x,
+			y:socket.y,
+			r:socket.r,
+			g:socket.g,
+			b:socket.b
+		});
+	}
+	for(var s2 in socketList){
+		var socket2 = socketList[s2];
+		socket2.emit('nP',pack);
+	}
+
+
+
+
+},1000/25);
