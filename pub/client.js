bcbdb12219af05c3f9d026d550175805f5db6dbd
William <devwb@outlook.com>
2016-08-01 15:58:03 +0100

    Clientside chat/game separation

--- pub/client.js
+++ pub/client.js
@@ -40,24 +40,6 @@ socket.on('regResponse', function(response) {
     }
 });
 
-
-/**
- * Chat
- *
- */
-var chatOutput = document.getElementById("chat-output");
-var chatInput = document.getElementById("chat-input");
-var chatForm = document.getElementById("chat-form");
-socket.on('serverMessage', function(message) {
-    chatOutput.innerHTML += "<div STYLE='color:white; font: 18px bold arial'>" + message + "</div>"
-});
-chatForm.onsubmit = function(e) {
-    e.preventDefault();
-
-    socket.emit('clientMessage', chatInput.value);
-    chatInput.value = '';
-};
-
 /**
  * Game
  */
