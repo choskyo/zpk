049db98e34c7d786926614f54a786eea4e2cb4b2
William <devwb@outlook.com>
2016-08-04 20:44:10 +0100

    Mobile detection + controls

--- pub/chat.js
+++ pub/chat.js
@@ -1,17 +1,21 @@
 /**
  * Created by will on 01/08/16.
  */
-
-
+var chatDiv = document.getElementById("chat");
 var chatOutput = document.getElementById("chat-output");
 var chatInput = document.getElementById("chat-input");
 var chatForm = document.getElementById("chat-form");
-socket.on('serverMessage', function(message) {
-    chatOutput.innerHTML += "<div STYLE='color:white; z-index: 2; opacity:1 ;font: 18px bold arial'>" + message + "</div>"
-});
-chatForm.onsubmit = function(e) {
-    e.preventDefault();
+if(!isMobile) {
+    socket.on('serverMessage', function(message) {
+        chatOutput.innerHTML += "<div STYLE='color:white; z-index: 2; opacity:1 ;font: 18px bold arial'>" + message + "</div>"
+    });
+    chatForm.onsubmit = function(e) {
+        e.preventDefault();
 
-    socket.emit('clientMessage', chatInput.value);
-    chatInput.value = '';
-};
+        socket.emit('clientMessage', chatInput.value);
+        chatInput.value = '';
+    };
+}
+else {
+    chatDiv.style.display = 'none';
+}
